e7bb7ed060e10fa26188edb860ec8ccbf8992b3f
RaviTejaMiddi <medhi.ravi@gmail.com>
2025-08-11 09:39:38 -0500

    lc-75'

--- src/twopointer/SortColors.java
+++ src/twopointer/SortColors.java
@@ -4,8 +4,21 @@ public class SortColors {
     public static void main(String[] args) {
         sortColors(new int[]{2,0,2,1,1,0});
     }
-    public  static void sortColors(int[] nums) {
-        //solve
+    public static void sortColors(int[] nums) {
+
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[j]<nums[i]) {
+                    int temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+        }
+        for(int val:nums){
+            System.out.print(val + " ");
+        }
+
         }
 
 
