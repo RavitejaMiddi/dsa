96595bca3fca060eb479e1fc4c7ede052393f06a
RaviTejaMiddi <medhi.ravi@gmail.com>
2025-08-11 09:52:54 -0500

    lc-75 approach two added

--- src/twopointer/SortColors.java
+++ src/twopointer/SortColors.java
@@ -5,19 +5,35 @@ public class SortColors {
         sortColors(new int[]{2,0,2,1,1,0});
     }
     public static void sortColors(int[] nums) {
+        int countZero = 0;
+        int countOne = 0;
 
         for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[j]<nums[i]) {
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                }
+            if(nums[i] == 0)
+                countZero++;
+            else if(nums[i] == 1)
+                countOne++;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(countZero > 0) {
+                nums[i] = 0;
+                countZero--;
             }
+            else if(countOne > 0) {
+                nums[i] = 1;
+                countOne--;
+            }
+            else nums[i] = 2;
         }
-        for(int val:nums){
-            System.out.print(val + " ");
-        }
+//        for(int i=0;i<nums.length;i++){
+//            for(int j=i+1;j<nums.length;j++){
+//                if(nums[j]<nums[i]) {
+//                    int temp = nums[i];
+//                    nums[i] = nums[j];
+//                    nums[j] = temp;
+//                }
+//            }
+//        }
 
         }
 
